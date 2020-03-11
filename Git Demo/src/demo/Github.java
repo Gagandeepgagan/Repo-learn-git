092d0f0edb7056706dc68a711f6f9c17dda28318
Gagandeepgagan <netzwelt@NW-GAGANQA-PC>
2020-03-11 16:29:13 +0530

    First commit

---
+++ Git Demo/src/demo/Github.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class Github {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("GitHub is built for collaboration. Set up an organization "
+		+ "to improve the way your team works together, and get access to more features.");
+	}
+
+}
